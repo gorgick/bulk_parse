8ef6c5bd71d9615b573fdfd05cf846714d1da5e4
gorgick <gorgick2014@yandex.by>
2026-02-20 19:10:21 +0300

    model

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    supplier = models.CharField(max_length=150)
+    rating = models.FloatField(blank=True, null=True)
+    text = models.TextField(null=True)
+
+    def __str__(self):
+        return self.name
